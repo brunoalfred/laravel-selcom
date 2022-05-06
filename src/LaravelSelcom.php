accecde73476cc46c4059033d52c47499f6c1cdb
brunoalfred <alfredybruno@gmail.com>
2022-05-06 16:23:39 +0300

    initial commit

---
+++ src/LaravelSelcom.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace JasiriLabs\LaravelSelcom;
+
+use Illuminate\Support\Facades\Http;
+use JetBrains\PhpStorm\ArrayShape;
+
+class LaravelSelcom
+{
+    
+    protected array $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config['laravel-selcom'];
+    }
+
+
+    public function computeSignature($data, $signed_fields, $requestTimestamp): string
+    {
+        $fieldsOrder = explode(',', $signed_fields);
+        $signData = "timestamp=$requestTimestamp";
+
+        foreach ($fieldsOrder as $key) {
+            $signData .= "&$key=" . $data[$key];
+        }
+
+        return base64_encode(hash_hmac('sha256', $signData, $this->config['api_secret'], true));
+    }
+
+
+    #[ArrayShape(['payment_gateway_url' => "false|string"])]
+
+    public function sendPostRequest($url, array $data, $digest, $signed_fields): array
+    {
+        date_default_timezone_set('Africa/Dar_es_Salaam');
+        $requestTimestamp = date('c');
+
+        $endpointUrl = $this->config['base_url'] . $url;
+
+        $signed_fields  = implode(',', array_keys($data));
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json;charset=\"utf-8\"',
+            'Accept' => 'application/json',
+            'Cache-Control' => 'no-cache',
+            'Authorization' => 'SELCOM ' . base64_encode($this->config['api_key']),
+            'Digest-Method' => 'HS256',
+            'Digest' => $this->computeSignature($data, $signed_fields, $requestTimestamp),
+            'Timestamp' => $requestTimestamp,
+            'Signed-Fields' => $signed_fields,
+        ])->post($endpointUrl, $data);
+
+        $paymentGatewayUrl = base64_decode(json_decode($response->body(), true)['data'][0]['payment_gateway_url']);
+
+        return [
+            'payment_gateway_url' => $paymentGatewayUrl,
+        ];
+    }
+
+}
---
+++ src/LaravelSelcomFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace JasiriLabs\LaravelSelcom;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Jasirilabs\LaravelSelcom\Skeleton\SkeletonClass
+ */
+class LaravelSelcomFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-selcom';
+    }
+}
